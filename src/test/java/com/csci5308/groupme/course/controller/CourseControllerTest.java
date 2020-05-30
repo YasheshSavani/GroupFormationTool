8b423e56824d18b7de2a3f334635791b93ccd101
Yashesh Sureshchandra Savani <ys419823@dal.ca>
2020-05-30 03:43:54 -0300

    Remove Test function from CourseControllerTest

--- src/test/java/com/csci5308/groupme/course/controller/CourseControllerTest.java
+++ src/test/java/com/csci5308/groupme/course/controller/CourseControllerTest.java
@@ -1,14 +1,6 @@
 package com.csci5308.groupme.course.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
 
 class CourseControllerTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
 }
