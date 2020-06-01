f743f03cb243cd6bf6f5e4ee7bf5b1bea2198b90
Abhinav Mandava <ab425200@dal.ca>
2020-05-31 22:05:41 -0300

    Revert "Merge branch 'mandava/us7/admin_authentication_authorization' into 'master'"
    This reverts merge request !1

--- src/main/java/sql/UserQuery.java
+++
@@ -1,9 +0,0 @@
-package sql;
-
-public class UserQuery {
-
-	public static final String ADD_USER = "INSERT INTO user(userName, firstName, lastName, email, password) values (?,?,?,?,?)";
-	
-	public static final String ADD_USERROLE = "INSERT INTO userrole(userName, roleName) values (?, ?)";
-
-}
