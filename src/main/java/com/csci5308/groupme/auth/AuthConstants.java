5d579ef5fcc28f8d0b0729f3a80f7edd4a5249fc
Abhinav Mandava <ab425200@dal.ca>
2020-06-04 20:01:31 -0300

    mandava: refactor env var commnetd

--- src/main/java/com/csci5308/groupme/auth/AuthConstants.java
+++ src/main/java/com/csci5308/groupme/auth/AuthConstants.java
@@ -2,8 +2,8 @@ package com.csci5308.groupme.auth;
 
 public class AuthConstants {
 
-	//public static final String BASE_URL = "https://group19-test.herokuapp.com";
-	public static final String BASE_URL = System.getenv("BASE_URL");
+	public static final String BASE_URL = "https://group19-test.herokuapp.com";
+	//public static final String BASE_URL = System.getenv("BASE_URL");
 	public static final String SECRET_CODE = "21wyp882e81dgw5";
 	public static final String TEST_EMAIL = "abhinav.mandava78@gmail.com";
 }
